70f8ab1e3d73dfdd113b45e8350afdf1687b756c
D-future0 <oka4collins@gmail.com>
2023-02-15 20:18:37 +0100

    my first js project commit

---
+++ index.js
@@ -0,0 +1,22 @@
+// How to display code in you wed browser
+document.write('Document.write ');
+window.prompt('window.prompth');
+window.alert('window.alert');
+console.log('console.log');
+
+/* Muti-line 
+comment*/
+
+const pi = 3.14;
+let firstName;
+var lastName;
+firstName = 'collins';
+lastName = 'okafor';
+fullName = firstName + ' ' + lastName
+
+
+document.write(fullName);
+window.prompt(fullName);
+window.alert(fullName);
+console.log(fullName);
+console.log('pi constant: ' + pi);
